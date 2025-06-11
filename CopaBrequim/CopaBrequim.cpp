e6f0c470346a7f614cd9d067159b75c0b313e09a
lauramoroni <moronilaura28@gmail.com>
2025-06-11 10:04:39 -0300

    Adicionar arquivos de projeto.

---
+++ CopaBrequim/CopaBrequim.cpp
@@ -0,0 +1,86 @@
+/**********************************************************************************
+// CopaBrequim (Código Fonte)
+//
+// Criação:     10 Jun 2025
+// Atualização: 
+// Compilador:  Visual C++ 2022
+//
+// Descrição:   Jogo estilo plataforma usando a engine do curso
+//
+**********************************************************************************/
+
+#include "Engine.h"
+#include "CopaBrequim.h"
+
+// ------------------------------------------------------------------------------
+
+// inicializa membros estáticos da classe
+
+// ------------------------------------------------------------------------------
+
+void CopaBrequim::Init()
+{
+    // cria sistema de áudio
+    ;
+
+    // bounding box
+
+
+    // cria jogador
+
+
+    // inicializa nível de abertura do jogo
+
+}
+
+// ------------------------------------------------------------------------------
+
+void CopaBrequim::Update()
+{
+    // habilita/desabilita visualização da bounding box
+
+    // atualiza nível
+
+}
+
+// ------------------------------------------------------------------------------
+
+void CopaBrequim::Draw()
+{
+    // desenha nível
+}
+
+// ------------------------------------------------------------------------------
+
+void CopaBrequim::Finalize()
+{
+    // finaliza nível
+}
+
+
+// ------------------------------------------------------------------------------
+//                                  WinMain                                      
+// ------------------------------------------------------------------------------
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    Engine* engine = new Engine();
+
+    // configura o motor
+    engine->window->Mode(WINDOWED);
+    engine->window->Size(600, 300);
+    engine->window->Color(30, 50, 80);
+    engine->window->Title("CopaBrequim");
+    engine->window->Icon(IDI_ICON);
+    engine->window->Cursor(IDC_CURSOR);
+    //engine->graphics->VSync(true);
+
+    // inicia o jogo
+    int status = engine->Start(new CopaBrequim());
+
+    delete engine;
+    return status;
+}
+
+// ----------------------------------------------------------------------------
+
---
+++ CopaBrequim/CopaBrequim.h
@@ -0,0 +1,55 @@
+/**********************************************************************************
+// CopaBrequim (Arquivo de Cabeçalho)
+//
+// Criação:     10 Jun 2025
+// Atualização: 
+// Compilador:  Visual C++ 2022
+//
+// Descrição:   Jogo estilo plataforma usando a engine do curso
+//
+**********************************************************************************/
+
+#ifndef _COPABREQUIM_H_
+#define _COPABREQUIM_H_
+
+// ---------------------------------------------------------------------------------
+
+#include "Game.h"
+#include "Audio.h"
+#include "Resources.h"
+
+// ------------------------------------------------------------------------------
+
+// enum
+
+// ------------------------------------------------------------------------------
+
+class CopaBrequim : public Game
+{
+private:
+    static Game* level;            // nível atual do jogo
+
+public:
+    static bool viewBBox;           // estado da bounding box
+
+    void Init();                    // inicializa jogo
+    void Update();                  // atualiza lógica do jogo
+    void Draw();                    // desenha jogo
+    void Finalize();                // finaliza jogo
+
+    template<class T>
+    static void NextLevel()         // muda para próximo nível do jogo
+    {
+        if (level)
+        {
+            level->Finalize();
+            delete level;
+            level = new T();
+            level->Init();
+        }
+    };
+};
+
+// ---------------------------------------------------------------------------------
+
+#endif
